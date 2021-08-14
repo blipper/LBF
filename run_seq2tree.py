c57069581ddd755b09c39da64d6e3f46eaec296c
Martin Ross <mross_public@pobox.com>
2021-08-14 16:55:56 -0400

    Configure batch size

--- run_seq2tree.py
+++ run_seq2tree.py
@@ -36,6 +36,7 @@ parser.add_argument('--nstep', default=50, type=int, help='m-fix')
 parser.add_argument('--name', default='fix', type=str, help='model name')
 parser.add_argument('--number-of-problems', default='0', type=int, help='Number of training problems to use. 0 is all.')
 parser.add_argument('--cross-validate', default=True, type=bool, help='Cross validate to test accuracy')
+parser.add_argument('--batch_size', default=16, type=int, help='Batch size')
 
 
 options = parser.parse_args()
@@ -45,7 +46,7 @@ model_name = options.name
 number_of_problems = options.number_of_problems
 cross_validate = options.cross_validate
 
-batch_size = 64
+batch_size = options.batch_size
 embedding_size = 128
 hidden_size = 512
 n_epochs = 100
